8aafec01ea0e13da77427038b5a4c40086c19d5c
wzlu <442086331@qq.com>
2019-05-22 16:16:18 +0800

    edit base server

--- app/Server/base.php
+++ app/Server/base.php
@@ -4,5 +4,24 @@ namespace App\Server;
 
 class base
 {
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
 
+    public function sendRequest($method = 'GET')
+    {
+        $request = new \GuzzleHttp\Psr7\Request($method, $this->url);
+        $temp = $this;
+        $promise = $this->client->sendAsync($request)->then(function ($response) use ($temp) {
+            $temp->response = $response;
+            $temp->handle();
+        });
+        $promise->wait();
+    }
+
+    public function handle()
+    {
+        $this->{$this->requestType . 'handle'}();
+    }
 }
--- app/Server/search.php
+++ app/Server/search.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Server;
+
 use Sunra\PhpSimple\HtmlDomParser;
 use App\Model\book;
+
 /**
  * Class search
  * @package App\Server
@@ -15,48 +17,31 @@ class search extends base
 
     public function __construct()
     {
-        $this->client = new \GuzzleHttp\Client();
-        $this->dom= new HtmlDomParser();
+        parent::__construct();
     }
 
     public function getWebCate()
     {
         $url = $this->getWebCateUrl();
         $this->sendRequest();
-
     }
 
     public function getWebPage()
     {
 
     }
-    public function sendRequest($method = 'GET')
-    {
-        $request = new \GuzzleHttp\Psr7\Request($method, $this->url);
-        $temp = $this;
-        $promise = $this->client->sendAsync($request)->then(function ($response) use ($temp) {
-            $temp->response = $response;
-            $temp->handle();
-        });
-        $promise->wait();
-    }
+
     public function getWebCateUrl()
     {
-        $this->requestType='cate';
-        return ($this->url=$this->baseUrl . 'xiaoshuodaquan');
+        $this->requestType = 'cate';
+        return ($this->url = $this->baseUrl . 'xiaoshuodaquan');
     }
 
-    public function handle()
-    {
-        $this->{$this->requestType.'handle'}();
-    }
     public function cateHandle()
     {
-//        echo $this->response->getBody();
-//        die;
-        $html=HtmlDomParser::str_get_html($this->response->getBody());
-        foreach ($html->find('.novellist ul') as $ul){
-            foreach ($ul->find('li') as $li){
+        $html = HtmlDomParser::str_get_html($this->response->getBody());
+        foreach ($html->find('.novellist ul') as $ul) {
+            foreach ($ul->find('li') as $li) {
                 echo $li;
 //                die;
             }
