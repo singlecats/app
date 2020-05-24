494a5164af1480e9eb2f6c8babc9ae85744c6483
wzlu <442086331@qq.com>
2020-05-24 15:32:58 +0800

    master2

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -103,6 +103,7 @@ class IndexController extends Controller
 //        $http = new httpServer();
 //        $ret = $http->getList();
 //        print_r($ret);die;
+        echo 'master02';
         $http = new goodServer();
         echo $http->checkTime();
     }
